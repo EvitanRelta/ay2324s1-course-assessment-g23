5035e46db8c2b3b84c4354db2316361aecab959c
EvitanRelta <shauntanzongzhi@gmail.com>
2023-11-12 21:44:06 +0800

    Remove matching UI

--- frontend/src/pages/Questions.tsx
+++ frontend/src/pages/Questions.tsx
@@ -2,13 +2,10 @@ import { QuestionTable } from '../components/QuestionTable/QuestionTable.tsx'
 import Navbar from '../components/Navbar.tsx'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import MatchBar from '../components/MatchBar.tsx'
 import { useCurrentUser } from '../stores/userStore.ts'
 
 const Questions = () => {
     const { data: user } = useCurrentUser()
-    const isUser = user?.role === 'normal'
-
     const navigate = useNavigate()
 
     // Redirect if not logged in.
@@ -21,7 +18,6 @@ const Questions = () => {
         <>
             <Navbar />
             <div className='question-page-container'>
-                {isUser && <MatchBar />}
                 <QuestionTable />
             </div>
         </>
